890d141f419abbe27383ff524d514e08640ae58a
Eric <dasomi04@gmail.com>
2024-03-16 17:08:19 -0400

    feat: added some css

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -8,10 +8,10 @@ export default function ItemList() {
         <Item idx="01" title="SEASON 1" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
         text="Lorem ipsum dolor sit amet consectetuer adipiscing elit, sed do elusmod tempor incidunt ut labore el dolore"
         link="#item1"> </Item>
-        <Item idx="01" title="SEASON 1" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
+        <Item idx="02" title="WRISTBANDS" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
         text="Lorem ipsum dolor sit amet consectetuer adipiscing elit, sed do elusmod tempor incidunt ut labore el dolore"
         link="#item1"> </Item>
-        <Item idx="01" title="SEASON 1" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
+        <Item idx="03" title="STICKERS" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
         text="Lorem ipsum dolor sit amet consectetuer adipiscing elit, sed do elusmod tempor incidunt ut labore el dolore"
         link="#item1"> </Item>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,16 @@ import '../styles/Navbar.css'
 export default function Navbar () {
   return (
     <div class="horizontal-navbar">
-        <a href="#home">Home</a>
-        <a href="#collaborations">Collaborations</a>
-        <a href="#outreach">Outreach</a>
-        <a href="#stories">Stories</a>
-        <a href="#contactus">Contact Us</a>
+      <a class="navbar-logo" href="#home">
+        <img src="https://noemptyseats.org/cdn/shop/files/image.jpg?v=1686603145&width=500" alt="No Empty Seats Logo" />
+      </a>
+      <div class="navbar-text">
+        <a class="navbar-text" href="#collaborations">Collaborations</a>
+        <a class="navbar-text" href="#outreach">Outreach</a>
+        <a class="navbar-text" href="#stories">Stories</a>
+        <a class="navbar-text" href="#contactus">Contact Us</a>
+      </div>
+      
     </div>
   );
 }
