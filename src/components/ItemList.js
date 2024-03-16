43996705127f52da34a24807a8ea9aa98a77f842
Eric <dasomi04@gmail.com>
2024-03-16 18:17:50 -0400

    feat: css for navbar and item

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,13 +5,13 @@ import Item from './Item'
 export default function ItemList() {
   return (
     <div className = "itemlist">
-        <Item idx="01" title="SEASON 1" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
+        <Item idx="01" title="SEASON 1" image='https://noemptyseats.org/cdn/shop/files/blueneck.heic?v=1702417776&width=990' 
         text="Lorem ipsum dolor sit amet consectetuer adipiscing elit, sed do elusmod tempor incidunt ut labore el dolore"
         link="#item1"> </Item>
-        <Item idx="02" title="WRISTBANDS" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
+        <Item idx="02" title="WRISTBANDS" image='https://noemptyseats.org/cdn/shop/files/IMG_4230.heic?v=1690921694&width=533' 
         text="Lorem ipsum dolor sit amet consectetuer adipiscing elit, sed do elusmod tempor incidunt ut labore el dolore"
         link="#item1"> </Item>
-        <Item idx="03" title="STICKERS" image="https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg" 
+        <Item idx="03" title="STICKERS" image='https://noemptyseats.org/cdn/shop/files/IMG_3268.heic?v=1686795342&width=533' 
         text="Lorem ipsum dolor sit amet consectetuer adipiscing elit, sed do elusmod tempor incidunt ut labore el dolore"
         link="#item1"> </Item>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,10 @@ export default function Navbar () {
         <img src="https://noemptyseats.org/cdn/shop/files/image.jpg?v=1686603145&width=500" alt="No Empty Seats Logo" />
       </a>
       <div class="navbar-text">
-        <a class="navbar-text" href="#collaborations">Collaborations</a>
-        <a class="navbar-text" href="#outreach">Outreach</a>
-        <a class="navbar-text" href="#stories">Stories</a>
-        <a class="navbar-text" href="#contactus">Contact Us</a>
+        <a class="navbar-text" href="#collaborations">COLLABORATIONS</a>
+        <a class="navbar-text" href="#outreach">OUTREACH</a>
+        <a class="navbar-text" href="#stories">STORIES</a>
+        <a class="navbar-text" href="#contactus">CONTACT</a>
       </div>
       
     </div>
